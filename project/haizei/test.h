0070fae1ce89388821fae35949a2c0b385a3e70f
chenzheyu <2287441467@qq.com>
2020-10-25 10:30:15 +0800

    first version

---
+++ project/haizei/test.h
@@ -0,0 +1,37 @@
+/*************************************************************************
+	> File Name: test.h
+	> Author: 少年宇
+	> Mail: 
+	> Created Time: 2020年10月24日 星期六 09时54分41秒
+ ************************************************************************/
+
+#ifndef _TEST_H
+#define _TEST_H
+
+#define TEST(a, b) \
+void a##_haizei_##b();\
+__attribute__((constructor))\
+void add##_haizei_##a##_haizei_##b() {\
+    add_function(a##_haizei_##b, #a "_haizei_" #b);\
+}\
+void a##_haizei_##b()
+//1st bug:重复定义的问题，解决：中间加字符_haizei_
+
+#define EXPECT_EQ(a, b) \
+printf("%s == %s ? %s\n", #a, #b, (a) == (b) ? "True" : "False");
+
+
+typedef void (*TestFuncT)();
+struct Function {
+    TestFuncT func;
+    const char *str;
+};
+
+
+
+int RUN_ALL_TESTS();
+void add_function(TestFuncT, const char *);
+
+
+
+#endif
---
+++ project/main.cpp
@@ -0,0 +1,41 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 少年宇
+	> Mail: 
+	> Created Time: 2020年10月23日 星期五 22时18分14秒
+ ************************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <vector>
+//#include <gtest/gtest.h>
+#include <haizei/test.h>
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+} 
+
+TEST(testFunc, add) {
+    EXPECT_EQ(add(5, 3), 8);
+    //ASSERT_EQ(add(5, 3), 9);
+    EXPECT_EQ(add(6, 7), 12);
+}
+
+TEST(test, Funcadd) {
+    EXPECT_EQ(add(5, 3), 9);
+    EXPECT_EQ(add(6, 7), 15);
+}
+
+int main(int argc, char *argv[]) {
+    return RUN_ALL_TESTS();
+}
+
